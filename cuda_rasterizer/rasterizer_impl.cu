4bdf94e0383f037d5da8c785d458fbc21786d1c0
tianqi <tianqi@tianqideMacBook-Pro.local>
2026-07-22 14:49:39 +0800

    modify

--- cuda_rasterizer/rasterizer_impl.cu
+++ cuda_rasterizer/rasterizer_impl.cu
@@ -161,7 +161,7 @@ CudaRasterizer::GeometryState CudaRasterizer::GeometryState::fromChunk(char*& ch
 	obtain(chunk, geom.clamped, P * 3, 128);
 	obtain(chunk, geom.internal_radii, P, 128);
 	obtain(chunk, geom.means2D, P, 128);
-	obtain(chunk, geom.cov3D, P * 6, 128);·
+	obtain(chunk, geom.cov3D, P * 6, 128);
 	obtain(chunk, geom.conic_opacity, P, 128);
 	obtain(chunk, geom.rgb, P * 3, 128);
 	obtain(chunk, geom.tiles_touched, P, 128);
